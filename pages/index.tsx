22058c5f39fe944f37e01722aaea8b69388d5108
Ioana <39728009+IoanaB82@users.noreply.github.com>
2020-12-17 20:21:56 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,7 @@ export default function Home() {
       <p>
         Plan ahead what to buy by adding the missing ingredients on the list.
       </p>
-      Delete all your saved data? Click here
-      <DeleteIcon
-        style={{ cursor: "pointer" }}
-        color="primary"
-        onClick={() => window.localStorage.removeItem("state")}
-      />
+      
     </div>
   );
 }
